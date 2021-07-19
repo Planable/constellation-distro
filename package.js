462585da77b6d5671983cf2537640951773b65b6
Nick <nick@planable.io>
2021-07-19 16:04:50 +0300

    👌 Update meteor version

--- package.js
+++ package.js
@@ -1,28 +1,26 @@
 Package.describe({
-  name: 'babrahams:constellation',
-  version: '0.4.11',
-  summary: 'A curated set of packages for Constellation',
-  git: 'https://github.com/JackAdams/constellation-distro.git',
-  documentation: 'README.md',
-  debugOnly: true
+  name: "planable:constellation",
+  version: "0.4.12",
+  summary: "A curated set of packages for Constellation",
+  git: "https://github.com/Planable/constellation-distro.git",
+  documentation: "README.md",
+  debugOnly: true,
 });
 
-Package.onUse(function(api) {
-	
-  api.versionsFrom('1.0');
+Package.onUse(function (api) {
+  api.versionsFrom("2.3");
 
-  api.use('constellation:console@1.4.7');
-  api.use('babrahams:temple@0.4.7');
-  api.use('constellation:subscriptions@0.4.7');
-  api.use('constellation:session@0.4.7');
-  api.use('constellation:autopublish@0.4.7');
-  api.use('constellation:tiny@0.4.7');
-  api.use('constellation:position@0.4.7');
+  api.use("constellation:console@1.4.7");
+  api.use("babrahams:temple@0.4.7");
+  api.use("constellation:subscriptions@0.4.7");
+  api.use("constellation:session@0.4.7");
+  api.use("constellation:autopublish@0.4.7");
+  api.use("constellation:tiny@0.4.7");
+  api.use("constellation:position@0.4.7");
   // api.use('lai:ddp-inspector@1.1.9');
-  api.use('constellation:plugins@0.4.9');
-  
-  api.imply('constellation:console');
-  
+  api.use("constellation:plugins@0.4.9");
+
+  api.imply("constellation:console");
 });
 
 Package.onTest(function(api) {
